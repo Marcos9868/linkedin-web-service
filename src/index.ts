62182d7682aaf65b535292cf087a39a58d442ad9
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-09-20 17:20:22 -0300

    Feat: add cors and json configurations on web service

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const service = express();
+
+service.use(cors());
+service.use(express.json());
+service.use(express.urlencoded({ extended: true }));
 
 service.listen(4000, () => console.log('Web Service running...'));
